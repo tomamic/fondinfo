7a3f63f5a95b432dc23ac5ed5f620a9699af762f
Michele Tomaiuolo <tomamic@ce.unipr.it>
2011-11-25 10:46:50 +0100

    license

--- 2008-dom/main.cpp
+++ 2008-dom/main.cpp
@@ -1,8 +1,8 @@
-/*
+/**
  * Example used in programming courses at University of Parma, IT.
- * Author: Michele Tomaiuolo - tomamic@ce.unipr.it - 2008
+ * Author: Michele Tomaiuolo - <tomamic@ce.unipr.it> - 2008
  *
- * This is free software: you can redistribute it and/or modify it
+ * This software is free: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License, version 3 or
  * later. See <http://www.gnu.org/licenses/>.
  */
