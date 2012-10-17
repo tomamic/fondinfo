7b891031c8ee488f85afcbcf20dad01bf7881b23
Mic <mic@mic-Studio-XPS-1340.(none)>
2012-10-17 16:05:11 +0200

    simplified code

--- 2010-e3-3-scytale/main.cpp
+++ 2010-e3-3-scytale/main.cpp
@@ -16,29 +16,26 @@ using namespace std;
 
 int main(int argc, char*argv[]) {
 
-    //ifstream in("../2010-3-3-scytale/main.cpp");
-    //istringstream in("Il C++ e` un linguaggio di programmazione orientato agli oggetti, con tipizzazione statica. E` stato sviluppato (in origine col nome di 'C con classi' da Bjarne Stroustrup ai Bell Labs nel 1983 come un miglioramento del linguaggio C. Tra i miglioramenti principali troviamo: l'introduzione del paradigma di programmazione a oggetti (le classi), funzioni virtuali, overloading degli operatori, ereditarieta` multipla, template e gestione delle eccezioni.");
-
     ifstream in("../2010-e3-3-scytale/main.cpp");
     ofstream out("out.txt");
-    const int M = 3, N = 4;
-    char matrix[M][N]; // M, N should be constants! (standard C++)
-    // vector<vector<char> > matrix(M, vector<char>(N));
+    in >> noskipws;
+
+    const char FILLER = ' ';
+    const int ROWS = 3;
+    const int COLS = 4;
+    vector<vector<char> > matrix(ROWS, vector<char>(COLS));
 
     while (in.good()) {
-        for (int y = 0; y < M; ++y) {
-            for (int x = 0; x < N; ++x) {
-                char c; in.get(c);
-                if (in.good()) {
-                    matrix[y][x] = c;
-                } else {
-                    matrix[y][x] = ' ';
-                }
+        for (int y = 0; y < ROWS; ++y) {
+            for (int x = 0; x < COLS; ++x) {
+                char c = FILLER;
+                in >> c; // is "in" still good?
+                matrix[y][x] = c;
             }
         }
 
-        for (int x =0; x < N; ++x) {
-            for (int y = 0; y < M; ++y) {
+        for (int x =0; x < COLS; ++x) {
+            for (int y = 0; y < ROWS; ++y) {
                 out << matrix[y][x];
             }
         }
