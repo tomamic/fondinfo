1261523a030fe752234e592ebe1a8f75b8e2f109
mic <mic@mic-Studio-XPS-1340.(none)>
2012-10-26 12:53:59 +0200

    recursion, memoization, iteration

--- 2010-fibonacci/main.cpp
+++ 2010-fibonacci/main.cpp
@@ -45,7 +45,7 @@ int fibonacci3(int n)
     for (int i = 2; i <= n; i++) {
         result += fib2;
 
-        // store previous value of result, for next step
+        // store previous result, for next step
         fib2 = result - fib2;
     }
 
