4958baa91ed7727a40a9ee77f03df7cd3220f544
mic <tomamic@ce.unipr.it>
2013-07-30 00:45:08 +0200

    2013 examples
    Former-commit-id: d0cc5a1694533773580821cc7e37a0a2f1ce9d3f

--- cpp/notepad/notepadwindow.cpp
+++ cpp/notepad/notepadwindow.cpp
@@ -5,27 +5,32 @@
 #include <QMenu>
 #include <QMenuBar>
 #include <QToolBar>
+#include <QStyle>
 
 NotepadWindow::NotepadWindow(QWidget *parent)
     : QMainWindow(parent)
 {
-    Notepad* notepad = new Notepad; setCentralWidget(notepad);
+    auto notepad = new Notepad; setCentralWidget(notepad);
 
-    QMenu* fileMenu = menuBar()->addMenu(tr("&File"));
-    QAction* openAction = fileMenu->addAction(tr("&Open"));
-    QAction* saveAction = fileMenu->addAction(tr("&Save"));
-    fileMenu->addSeparator();
-    QAction* exitAction = fileMenu->addAction(tr("E&xit"));
+    auto menu = menuBar()->addMenu(tr("&File"));  // QMenu*
+    auto openAct = menu->addAction(tr("&Open"));  // QAction*
+    auto saveAct = menu->addAction(tr("&Save"));
+    menu->addSeparator();
+    auto exitAct = menu->addAction(tr("E&xit"));
 
-    connect(openAction, &QAction::triggered, notepad, &Notepad::open);
-    connect(saveAction, &QAction::triggered, notepad, &Notepad::save);
-    connect(exitAction, &QAction::triggered, notepad, &Notepad::exit);
+    openAct->setIcon(style()->standardIcon(QStyle::SP_DialogOpenButton));
+    saveAct->setIcon(style()->standardIcon(QStyle::SP_DialogSaveButton));
+    exitAct->setIcon(style()->standardIcon(QStyle::SP_DialogCloseButton));
 
-//    QToolBar* toolBar = addToolBar(tr("&File"));
-//    toolBar->addAction(openAction);
-//    toolBar->addAction(saveAction);
-//    toolBar->addSeparator();
-//    toolBar->addAction(exitAction);
+    auto tools = addToolBar(tr("&File"));  // QToolBar*
+    tools->addAction(openAct);
+    tools->addAction(saveAct);
+    tools->addSeparator();
+    tools->addAction(exitAct);
+
+    connect(openAct, SIGNAL(triggered()), notepad, SLOT(open()));
+    connect(saveAct, SIGNAL(triggered()), notepad, SLOT(save()));
+    connect(exitAct, SIGNAL(triggered()), notepad, SLOT(exit()));
 }
 
 NotepadWindow::~NotepadWindow()
