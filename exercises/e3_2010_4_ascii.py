38841a5e3f9152e8b433c7ebf818c4373246255d
mic <tomamic@ce.unipr.it>
2013-10-17 11:49:57 +0200

    less spaces
    Former-commit-id: 35da1e24df066819593f5c7ca03cd2db03e8c61b

--- exercises/e3_2010_4_ascii.py
+++ exercises/e3_2010_4_ascii.py
@@ -7,5 +7,5 @@ for y in range(ROWS):
     for x in range(COLS):
         i = FIRST + y + x * ROWS
         if i <= LAST:
-            print('{:4}'.format(i), chr(i), end='')
+            print('{:3} {} '.format(i, chr(i)), end='')
     print()
