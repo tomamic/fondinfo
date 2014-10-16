14656aa9793e104cc7047aaebd65b0ede9dbf37b
Mic <tomamic@ce.unipr.it>
2014-10-16 11:42:18 +0200

    pixel-based ball
    Former-commit-id: 6d1b4663d194f429364a1800d4752240ffa3c9e2

--- examples/p4_ball.py
+++ examples/p4_ball.py
@@ -8,36 +8,36 @@
 import sys
 
 class Ball:
-    ARENA_WIDTH = 16
-    ARENA_HEIGHT = 12
+    W, H = 20, 20
+    ARENA_W, ARENA_H = 320, 240
 
     def __init__(self, x: int, y: int):
         self._x = x
         self._y = y
-        self._dx = +1
-        self._dy = +1
-        
+        self._dx = 5
+        self._dy = 5
+
     def move(self):
-        if not (0 <= self._x + self._dx < Ball.ARENA_WIDTH):
-            self._dx = -self._dx
-        if not (0 <= self._y + self._dy < Ball.ARENA_HEIGHT):
-            self._dy = -self._dy
         self._x += self._dx
         self._y += self._dy
+        if not (0 <= self._x < Ball.ARENA_W - Ball.W):
+            self._dx = -self._dx
+        if not (0 <= self._y < Ball.ARENA_H - Ball.H):
+            self._dy = -self._dy
 
-    def position(self) -> (int, int):
-        return (self._x, self._y)
+    def rect(self) -> (int, int, int, int):
+        return self._x, self._y, Ball.W, Ball.H
 
 
 if __name__ == '__main__':
     # Create two objects, instances of the Ball class
-    b1 = Ball(4, 8)
-    b2 = Ball(8, 4)
-    print('Ball 1 @', b1.position())
-    print('Ball 2 @', b2.position())
+    b1 = Ball(40, 80)
+    b2 = Ball(80, 40)
+    print('Ball 1 @', b1.rect())
+    print('Ball 2 @', b2.rect())
 
     for line in sys.stdin:
         b1.move()
         b2.move()
-        print('Ball 1 @', b1.position())
-        print('Ball 2 @', b2.position())
+        print('Ball 1 @', b1.rect())
+        print('Ball 2 @', b2.rect())
