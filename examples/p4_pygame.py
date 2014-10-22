1b428ed79da4ce61298e5ec8afacd94243384128
Mic <tomamic@ce.unipr.it>
2014-10-22 14:48:30 +0200

    pygame example
    Former-commit-id: 7e9652a583f4c595e5c5ac67d1f3757a58208f7e

---
+++ examples/p4_pygame.py
@@ -0,0 +1,19 @@
+from p4_ball import Ball
+import pygame
+pygame.init()                     # Prepare pygame
+clock = pygame.time.Clock()       # To set game speed
+screen = pygame.display.set_mode((Ball.ARENA_W, Ball.ARENA_H))
+
+b = Ball(40, 80)
+playing = True
+while playing:
+    for e in pygame.event.get():  # Handle events: mouse, keyb etc.
+        if e.type == pygame.QUIT: playing = False
+    screen.fill((255, 255, 255))
+
+    b.move()
+    pygame.draw.rect(screen, (127, 127, 127), b.rect())
+
+    pygame.display.flip()         # Surface ready, show it!
+    clock.tick(30)                # Delay to get 30 fps
+pygame.quit()                     # Close the window
