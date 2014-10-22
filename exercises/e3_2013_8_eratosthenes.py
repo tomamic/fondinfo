5b6053784aa64f10e4846ef966a624b2795ee713
Mic <tomamic@ce.unipr.it>
2014-10-22 12:10:13 +0200

    minor
    Former-commit-id: 052d397df582084652cc64051888d5964ddf0bf3

--- exercises/e3_2013_8_eratosthenes.py
+++ exercises/e3_2013_8_eratosthenes.py
@@ -6,22 +6,21 @@ def primes1(n: int):
     while j < len(nums):
         x = nums[j]
         for i in range(x, n // x + 1):
-            if x * i in nums:
+            try:  # if x * i in nums
                 nums.remove(x * i)
+            except:
+                pass
         j += 1
     return nums
 
 def primes2(n: int):
+    nums = []
     is_prime = [True] * (n + 1)
     for x in range(2, n + 1):
         if is_prime[x]:
+            nums.append(x)
             for i in range(x, n // x + 1):
                 is_prime[x * i] = False
-    nums = []
-    for i in range(2, n + 1):
-       if is_prime[i]:
-           nums.append(i)
-    # nums = [i for (i, val) in enumerate(is_prime) if i > 1 and val]
     return nums
 
 if __name__ == '__main__':
