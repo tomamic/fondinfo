6781a726a3a9113999e890153e33426cbbde0d7c
mic <tomamic@ce.unipr.it>
2014-10-27 13:44:38 +0100

    added text
    Former-commit-id: 2eb3e3e338865263e09fdb8e75eefae44b29f208

--- exercises/e4_2010_4_hanoi.py
+++ exercises/e4_2010_4_hanoi.py
@@ -1,21 +1,28 @@
-DISCS = 5
-POLES = 3
-SUM_OF_POLE_IDS = 2 + 1 + 0
+'''
+@author Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+
+4.4 Torre di Hanoi
+* Tre paletti + N dischi di diametro decrescente
+* Portare tutti i dischi dal primo all'ultimo paletto
+* Si può spostare solo un disco alla volta
+* Non si può mettere un disco su uno più piccolo
+(Usare la ricorsione. Immediato spostare un solo disco.
+N dischi: spostarne N-1 sul piolo né origine né dest., spostare l'ultimo disco sul piolo giusto, spostare ancora gli altri N-1 dischi.)
+'''
 
 def print_towers(towers: list):
-    for t in range(POLES):
+    for t in towers:
         print('|-', end='')
-        for d in towers[t]:
+        for d in t:
             print(d, end='')
         print()
     input()
 
-def move_towers(towers: list, n: int, src: int, dst: int):
-    # find the teporary pole (for this plan)
-    tmp = SUM_OF_POLE_IDS - src - dst;
-
+def move_towers(towers: list, n: int, src: int, tmp: int, dst: int):
     # if there are discs above, move n-1 away
-    if n > 1: move_towers(towers, n-1, src, tmp);
+    if n > 1:
+        move_towers(towers, n - 1, src, dst, tmp);
 
     # now move the largest disc (of n) to its dest    
     top_disc = towers[src].pop()
@@ -23,13 +30,16 @@ def move_towers(towers: list, n: int, src: int, dst: int):
     print_towers(towers)
 
     # if there were discs above, move those on top
-    if n > 1: move_towers(towers, n-1, tmp, dst)
+    if n > 1:
+        move_towers(towers, n - 1, tmp, src, dst)
 
 if __name__ == '__main__':
+    DISCS = 5
+    POLES = 3
     towers = [[] for p in range(POLES)]
     for d in reversed(range(DISCS)):
         towers[0].append(d + 1)
     print_towers(towers)
 
     # move all discs from pole 0 to pole 2
-    move_towers(towers, DISCS, 0, POLES-1)
+    move_towers(towers, DISCS, 0, 1, 2)
