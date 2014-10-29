fa1468e6836517ce68e80966164408274715dcde
mic <tomamic@ce.unipr.it>
2014-10-29 16:48:28 +0100

    polimorphism: animals
    Former-commit-id: 29bc208ab9416d8908e1aba4d27aec5fe0125469

---
+++ examples/p4_animals.py
@@ -0,0 +1,31 @@
+class Animal:
+    def say(self):
+        raise NotImplementedError("Abstract method")
+
+class Dog(Animal):
+    def __init__(self, name):
+        self._name = name
+
+    def say(self):
+        print(self._name, 'says :',  'bau')
+
+class Cat(Animal):
+    def __init__(self, name):
+        self._name = name
+
+    def say(self):
+        print(self._name, 'says :',  'miao')
+
+class Pig(Animal):
+    def __init__(self, name):
+        self._name = name
+
+    def say(self):
+        print(self._name, 'says :',  'oink')
+
+
+# a list of Animal objects
+animals = [Dog('danny'), Cat('candy'), Pig('peppa'), Pig('george')]
+
+for a in animals:
+    a.say()
