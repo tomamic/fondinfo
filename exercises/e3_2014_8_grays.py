8781b8cdab4207253a0fd26e51d4b6b8d655eda0
mic <tomamic@ce.unipr.it>
2014-11-06 11:41:28 +0100

    new exercise: grays
    Former-commit-id: a9a1808f89f3d539a858ed23d6588c2c2b139069

---
+++ exercises/e3_2014_8_grays.py
@@ -0,0 +1,26 @@
+import pygame
+
+grays = []
+val = int(input('gray? '))
+while 0 <= val < 256:
+    grays.append(val)
+    val = int(input('gray? '))
+
+m = int(input('repetitions? '))
+
+side = 400.0
+delta = side / (len(grays) * m)
+
+pygame.init()
+w = pygame.display.set_mode((int(side), int(side)))
+
+for i in range(m):
+    for val in grays:
+        pygame.draw.rect(w, (val, val, val),
+                         (0, 0, int(side), int(side)))
+        side -= delta
+
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
