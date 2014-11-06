7415461dea9829ef70963ce176efbb3125a8e7e1
mic <tomamic@ce.unipr.it>
2014-11-06 11:44:33 +0100

    new exercise: palette
    Former-commit-id: 1e6a48f4c63f46923e5d4b3b9eb882a0932e5146

---
+++ exercises/e3_2014_9_palette.py
@@ -0,0 +1,26 @@
+import pygame
+
+palette = []
+with open('palette.txt', 'r') as f:
+    for line in f:
+        vals = line.split()
+        color = (int(vals[0]), int(vals[1]), int(vals[2]))
+        palette.append(color)
+
+n = int(input('squares? '))
+
+side = 400.0
+delta = side / n
+
+pygame.init()
+w = pygame.display.set_mode((int(side), int(side)))
+
+for i in range(n):
+    color = palette[i % len(palette)]
+    pygame.draw.rect(w, color, (0, 0, int(side), int(side)))
+    side -= delta
+
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
