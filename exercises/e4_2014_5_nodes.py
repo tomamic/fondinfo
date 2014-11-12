6916bda20cf3720dd66962619df743e5d4cc0a27
mic <tomamic@ce.unipr.it>
2014-11-12 14:47:06 +0100

    new exercise: nodes
    Former-commit-id: 11b9688ae7794555058649c5544cfcbcd4031351

---
+++ exercises/e4_2014_5_nodes.py
@@ -0,0 +1,56 @@
+class Node:
+    def size(self):
+        raise NotImplementedError("Abstract method")
+    
+    def print(self, indent: int):
+        raise NotImplementedError("Abstract method")
+
+class File(Node):
+    def __init__(self, name: str, text: str):
+        self._name = name
+        self._text = text
+
+    def size(self) -> int:
+        return len(self._text)
+
+    def print(self, indent: int):
+        print(' ' * indent, self._name, sep='')
+
+class Folder(Node):
+    def __init__(self, name: str):
+        self._name = name
+        self._subnodes = []
+        
+    def add_node(self, n: Node):
+        self._subnodes.append(n)
+        
+    def size(self) -> int:
+        total_size = 0
+        for n in self._subnodes:
+            total_size += n.size()
+        return total_size
+    
+    def print(self, indent: int):
+        print(' ' * indent, self._name, sep='')
+        for n in self._subnodes:
+            n.print(indent + 4)
+
+if __name__ == '__main__':
+    a1_0 = File('a1.txt', 'bla bla 0')
+    ball = File('ball.gif', 'an image')
+    data = Folder('data')
+    data.add_node(ball)
+    cmpt166 = Folder('cmpt166')
+    cmpt166.add_node(a1_0)
+    cmpt166.add_node(data)
+    a1_1 = File('a1.txt', 'a different file')
+    macm101 = Folder('macm101')
+    macm101.add_node(a1_1)
+    desktop = Folder('Desktop')
+    desktop.add_node(cmpt166)
+    desktop.add_node(macm101)
+
+    print(desktop.size())
+
+    print()
+    desktop.print(0)
