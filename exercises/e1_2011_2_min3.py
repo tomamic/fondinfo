88c6ae83f73b32a136dd90aef65ff74e82ce0ffe
mic <tomamic@ce.unipr.it>
2014-11-12 15:14:41 +0100

    comment
    Former-commit-id: 01279fc17c3bb9cd362f03975aadba44eaaece8b

--- exercises/e1_2011_2_min3.py
+++ exercises/e1_2011_2_min3.py
@@ -1,3 +1,14 @@
+'''
+@author Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+
+* Chiedere all'utente tre numeri interi: a, b, c
+* Determinare qual è il minore dei tre
+
+Controllare prima di tutto se a è minore degli altri due,
+altrimenti controllare quale è il minore tra b e c
+'''
+
 a = int(input())
 b = int(input())
 c = int(input())
