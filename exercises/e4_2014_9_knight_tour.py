042727bf9bc7c40d4bdc89dab7f6c117489924a7
mic <tomamic@ce.unipr.it>
2014-11-14 00:33:12 +0100

    border_distance
    Former-commit-id: cafab82d05866e07ad7da65d8c6f6257d9a758ee

--- exercises/e4_2014_9_knight_tour.py
+++ exercises/e4_2014_9_knight_tour.py
@@ -4,21 +4,20 @@ sys.setrecursionlimit(15000)
 delta = [(1, -2), (2, -1), (2, 1), (1, 2),
          (-1, 2), (-2, 1), (-2, -1), (-1, -2)]
 
-def find_moves(board, x, y) -> list:
+def find_moves(board, x0, y0) -> list:
+    w, h = len(board[0]), len(board)
     moves = []
     for d in delta:
-        xm = x + d[0]
-        ym = y + d[1]
-        if (0 <= xm < len(board[0]) and
-            0 <= ym < len(board) and
-            board[ym][xm] == 0):
-            moves.append((xm, ym))
+        x, y = x0 + d[0], y0 + d[1]
+        if (0 <= x < w and 0 <= y < h and board[y][x] == 0):
+            moves.append((x, y))
     return moves
 
-def distance_from_border(x, y, w, h):
+def border_distance(x, y, w, h):
     dist_x = min(x, w - x)
     dist_y = min(y, h - y)
-    return dist_x + dist_y
+    # return dist_x + dist_y
+    return sorted((dist_x, dist_y))
 
 def knight_tour(board, x, y, n) -> bool:
     board[y][x] = n
@@ -27,22 +26,19 @@ def knight_tour(board, x, y, n) -> bool:
     if n == w * h: return True
 
     moves = find_moves(board, x, y)
-##    moves.sort(key=lambda elem:
-##            distance_from_border(elem[0], elem[1], w, h))
-##    moves.sort(key=lambda elem:
-##               len(find_moves(board, elem[0], elem[1])))
+##    moves.sort(key=lambda m: border_distance(m[0], m[1], w, h))
+##    moves.sort(key=lambda m: len(find_moves(board, m[0], m[1])))
 
+    # try each possible move (recursively)
     for (xm, ym) in moves:
-            # move the knight this way
-            if knight_tour(board, xm, ym, n + 1): return True
+        if knight_tour(board, xm, ym, n + 1): return True
 
     # no luck this way, go back (backtracking)
     board[y][x] = 0
     return False
 
 if __name__ == '__main__':
-    size = int(input('size? '))
-    w, h = size, size
+    w = h = int(input('size? '))
     board = [[0 for x in range(w)] for y in range(h)]
 
     print(knight_tour(board, 0, 0, 1))
