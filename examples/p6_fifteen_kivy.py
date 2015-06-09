3bc2ec5d3413de94eb1d254dd7988c55271baade
mic <tomamic@ce.unipr.it>
2015-06-09 13:13:35 +0200

    gridlayout
    Former-commit-id: cf165afbd2af8a71d634887ae0758edd58ebd3f7

--- examples/p6_fifteen_kivy.py
+++ examples/p6_fifteen_kivy.py
@@ -2,46 +2,48 @@
 
 from sys import argv
 
-from kivy.app import App
+from kivy.app import runTouchApp
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
-
+from kivy.properties import ListProperty
+ 
 from p5_fifteen_puzzle import FifteenPuzzle
 
 
-class FifteenGui(App):
-    def build(self):
-        self._puzzle = FifteenPuzzle(3, 3)
-        self._buttons = []
-        grid = GridLayout(cols=self._puzzle.cols)
+class GridButton(Button):
+    grid_pos = ListProperty((0, 0))
+    def __repr__(self): return self.text
+
+class FifteenGui(GridLayout):
+    def __init__(self):
+        GridLayout.__init__(self, cols=3)
+        self._puzzle = FifteenPuzzle(3, 2)
         for y in range(self._puzzle.rows):
             for x in range(self._puzzle.cols):
-                b = Button(background_color=(0, 1, 0, 1))
-                b.bind(on_release=lambda btn, x=x, y=y:
-                           self.handle_click(x, y))
-                grid.add_widget(b)
-                self._buttons.append(b)
+                btn = GridButton(grid_pos=(x, y),
+                                 background_color=(0, 1, 0, 1))
+                btn.bind(on_release=self.handle_click)
+                self.add_widget(btn)
         self.update_buttons()
-        return grid
 
     def update_button(self, x: int, y: int):
         val = self._puzzle.get(x, y)
         symbol = chr(ord('A') + val - 1)
         if val == 0: symbol = ' '
 
-        b = self._buttons[y * self._puzzle.cols + x]
-        b.text = symbol
+        b = self.children[-1 - y * self._puzzle.cols - x]
+        b.text = symbol  # children are reversed, in Kivy
 
     def update_buttons(self):
         for y in range(self._puzzle.rows):
             for x in range(self._puzzle.cols):
                 self.update_button(x, y)
                 
-    def handle_click(self, x: int, y: int):
-        self._puzzle.move_pos(x, y)
-        self.update_button(*self._puzzle.blank)  # args unpacking
+    def handle_click(self, btn: Button):
+        self._puzzle.move_pos(*btn.grid_pos)     # args unpacking
+        self.update_button(*self._puzzle.blank)
         self.update_button(*self._puzzle.moved)
 
         if self._puzzle.finished:
@@ -51,7 +53,7 @@ class FifteenGui(App):
             popup.open()
             self._puzzle.shuffle()
             self.update_buttons()
-            
 
+            
 if __name__ == '__main__':
-    FifteenGui().run()
+    runTouchApp(FifteenGui())
