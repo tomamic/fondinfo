7b02e39cc193722a6e290b3e5a64eb14a13306d0
mic <tomamic@ce.unipr.it>
2015-06-11 14:47:18 +0200

    add-repository
    Former-commit-id: e7c0e75337adde4094d6ed89e19a5fcc5af87828

--- install-pygame.sh
+++ install-pygame.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install mercurial python3-pip python3-dev python3-numpy libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
-sudo pip3 install hg+http://bitbucket.org/pygame/pygame
+sudo add-apt-repository ppa:thopiekar/pygame
+sudo apt-get install python3-pygame
+
+# ... otherwise, uncomment the following lines:
+# sudo apt-get install mercurial python3-pip python3-dev python3-numpy libav-tools libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev
+# sudo pip3 install hg+http://bitbucket.org/pygame/pygame
