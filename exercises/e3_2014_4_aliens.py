d9cc9d381f11037830c3fd0cb8dff2dd3af0b968
mic <tomamic@ce.unipr.it>
2015-10-27 21:15:04 +0100

    rect
    Former-commit-id: 2dce6036c6d203433230ab1aa08ce3eb647ea204

--- exercises/e3_2014_4_aliens.py
+++ exercises/e3_2014_4_aliens.py
@@ -2,6 +2,7 @@ import pygame
 
 class Alien:
     SPEED = 5
+    W, H = 20, 20
     PATH_W = 150
 
     def __init__(self, x0: int, y0: int):
@@ -11,24 +12,20 @@ class Alien:
         self._dx = self.SPEED
 
     def move(self):
-        new_x = self._x + self._dx
-        if self._xmin <= new_x < self._xmin + self.PATH_W:
-            self._x = new_x
+        xmax = self._xmin + self.PATH_W - self.W
+        if self._xmin <= self._x + self._dx <= xmax:
+            self._x += self._dx
         else:
             self._dx = -self._dx
             self._y += self.SPEED
 
-    def pos_x(self):
-        return self._x
-
-    def pos_y(self):
-        return self._y
+    def rect(self):
+        return self._x, self._y, self.W, self.H
 
 
 pygame.init()                     # Prepare pygame
 screen = pygame.display.set_mode((320, 240))
 clock = pygame.time.Clock()       # To set game speed
-image = pygame.image.load('../arena/ball.png')
 
 aliens = [Alien(40, 40), Alien(80, 80), Alien(120, 40)]
 
@@ -40,7 +37,8 @@ while playing:
 
     for a in aliens:
         a.move()                # Apply game logic
-        screen.blit(image, (a.pos_x(), a.pos_y()))   # Draw foreground
+                                # Draw foreground
+        pygame.draw.rect(screen, (127, 127, 127), a.rect())
 
     pygame.display.flip()         # Surface ready, show it!
     clock.tick(30)                # Wait 1/30 seconds
