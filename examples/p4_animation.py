39cc4425fd41fe0a2cf86fff5917c30cbac96af4
mic <tomamic@ce.unipr.it>
2015-10-27 16:21:58 +0100

    animation example
    Former-commit-id: 182615a4538cd5f675fad19ba159b2f8d15be0f8

---
+++ examples/p4_animation.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()                     # Prepare pygame
+screen = pygame.display.set_mode((320, 240))
+clock = pygame.time.Clock()       # To set game speed
+
+x = 50; playing = True
+while playing:
+    for e in pygame.event.get():  # Handle events: mouse, keyb etc.
+        if e.type == pygame.QUIT: playing = False
+    screen.fill((255, 255, 255))  # Draw background, and then foreground
+    pygame.draw.rect(screen, (127, 127, 127), (x, 50, 20, 20))
+    x = (x + 5) % 320             # Update ball's position
+    pygame.display.flip()         # Surface ready, show it!
+    clock.tick(30)                # Wait 1/30 seconds
+pygame.quit()                     # Close the window
