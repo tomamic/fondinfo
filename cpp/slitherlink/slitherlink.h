a78c0f71f35c8a077d4a5fedf4acec671e5cfde7
mic <tomamic@ce.unipr.it>
2015-12-01 16:24:25 +0100

    slitherlink ctor
    Former-commit-id: ee6d35b6da302b5bcbbd335e5b17224ffcd3fee0

--- cpp/slitherlink/slitherlink.h
+++ cpp/slitherlink/slitherlink.h
@@ -8,6 +8,8 @@ class Slitherlink : public Game
 {
 public:
     Slitherlink();
+//    Slitherlink(std::string filename);
+//    void write(std::string filename) const;
     virtual void play_at(int x, int y);
     virtual int cols() const {return cols_; }
     virtual int rows() const { return rows_; }
@@ -15,20 +17,9 @@ public:
     virtual bool finished() const;
     virtual std::string message() const { return "Puzzle solved"; }
 private:
-    int cols_ = 11;
-    int rows_ = 11;
-    std::vector< std::vector<char> > board_ = {
-        {'+', ' ', '+', ' ', '+', ' ', '+', ' ', '+', ' ', '+'},
-        {' ', ' ', ' ', '3', ' ', '2', ' ', '2', ' ', ' ', ' '},
-        {'+', ' ', '+', ' ', '+', ' ', '+', ' ', '+', ' ', '+'},
-        {' ', ' ', ' ', '0', ' ', ' ', ' ', ' ', ' ', '2', ' '},
-        {'+', ' ', '+', ' ', '+', ' ', '+', ' ', '+', ' ', '+'},
-        {' ', ' ', ' ', '2', ' ', ' ', ' ', ' ', ' ', '1', ' '},
-        {'+', ' ', '+', ' ', '+', ' ', '+', ' ', '+', ' ', '+'},
-        {' ', ' ', ' ', '0', ' ', ' ', ' ', ' ', ' ', '2', ' '},
-        {'+', ' ', '+', ' ', '+', ' ', '+', ' ', '+', ' ', '+'},
-        {' ', ' ', ' ', '2', ' ', ' ', ' ', '2', ' ', ' ', ' '},
-        {'+', ' ', '+', ' ', '+', ' ', '+', ' ', '+', ' ', '+'} };
+    int cols_ = 0;
+    int rows_ = 0;
+    std::vector< std::vector<char> > board_;
 };
 
 #endif // SLITHERLINK_H
