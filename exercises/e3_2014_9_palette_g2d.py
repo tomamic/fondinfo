f63ccddb83847c510736ce0cbc4722bbae70dd7a
mic <tomamic@ce.unipr.it>
2016-10-03 23:04:40 +0200

    palette
    Former-commit-id: 9d3e17b5ec8300b42948f443e312d043d04d142d

--- exercises/e3_2014_9_palette_g2d.py
+++ exercises/e3_2014_9_palette_g2d.py
@@ -6,16 +6,14 @@
 
 from game2d import *
 
-palette_txt = '''180 120 60
-120 180 60
-120 60 180'''
-
 palette = []
-f = palette_txt.split('\n')
-for line in f:
-    vals = line.split()
-    color = (int(vals[0]), int(vals[1]), int(vals[2]))
-    palette.append(color)
+f = open('palette.txt', 'r')
+with f:
+    for line in f:
+        if len(line) > 0:
+            vals = line.split()
+            color = (int(vals[0]), int(vals[1]), int(vals[2]))
+            palette.append(color)
 
 n = int(input('squares? '))
 
