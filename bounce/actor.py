0d41399c8f8996fc99287ce2fd40d2202a0c66ad
mic <tomamic@ce.unipr.it>
2016-10-17 00:32:23 +0200

    py3
    Former-commit-id: 1b2f72dede8325a4d9dd5e94030873294875a292

--- bounce/actor.py
+++ bounce/actor.py
@@ -4,74 +4,65 @@
 @license This software is free - http://www.gnu.org/licenses/gpl.html
 '''
 
-class Actor(object):
-    '''Interface to be implemented by each game character'''
-    
+class Actor():
+    '''Interface to be implemented by each game character
+    '''
     def move(self):
-        '''Called by Arena, at the actor's turn'''
-        raise NotImplementedError('Abstract method')
-
-    def collide(self, other):
-        '''Called by Arena, when the actor collides with another one
-        Args:
-            other: Actor -- the other actor involved in the collision
+        '''Called by Arena, at the actor's turn
         '''
         raise NotImplementedError('Abstract method')
 
-    def rect(self):
-        '''Return the rectangle containing the actor, as a 4-tuple of ints
-        Returns:
-            (int, int, int, int) -- (left, top, width, height)
+    def collide(self, other: Actor):
+        '''Called by Arena, when the actor collides with another one.
+        The `other` arg is the other actor involved in the collision
         '''
         raise NotImplementedError('Abstract method')
 
-    def symbol(self):
+    def rect(self) -> (int, int, int, int):
+        '''Return the rectangle containing the actor, as a 4-tuple of ints:
+        (left, top, width, height)
+        '''
+        raise NotImplementedError('Abstract method')
+
+    def symbol(self) -> (int, int):
         '''Return (0, 0) or the (x, y) position of current sprite in a
-           larger image, containing more sprites
-        Returns:
-            (int, int) -- the position of current sprite
+        larger image, containing more sprites
         '''
         raise NotImplementedError('Abstract method')
 
 
-class Arena(object):
-    '''A generic 2D game, with a given size in pixels and a list of actors'''
-
-    def __init__(self, width, height):
-        '''Create an arena, with given dimensions
-        Args:
-            width: int -- width in pixels
-            height: int -- height in pixels
+class Arena():
+    '''A generic 2D game, with a given size in pixels and a list of actors
+    '''
+    def __init__(self, width: int, height: int):
+        '''Create an arena, with given dimensions in pixels
         '''
         self._w, self._h = width, height
         self._actors = []
 
-    def add(self, a):
+    def add(self, a: Actor):
         '''Register an actor into this arena
-        Args:
-            a: Actor
+        Actors are blitted in their order of registration
         '''
         if a not in self._actors:
             self._actors.append(a)
 
-    def remove(self, a):
+    def remove(self, a: Actor):
         '''Cancel an actor from this arena
-        Args:
-            a: Actor
         '''
         if a in self._actors:
             self._actors.remove(a)
 
     def move_all(self):
         '''Move all actors (through their own move method).
-        After each single move, collisions are checked and
-        The collide methods of both colliding actors are called
+        After each single move, collisions are checked and eventually
+        the collide methods of both colliding actors are called
         '''
         actors = list(reversed(self._actors))
         for a in actors:
             previous_pos = a.rect()
             a.move()
-            if a.rect() != previous_pos:
+            if a.rect() != previous_pos:  # optimize for stationary actors
                 for other in actors:
                     # reversed order, so actors drawn on top of others
                     # (towards the end of the cycle) are checked first
@@ -79,14 +70,9 @@ class Arena(object):
                             a.collide(other)
                             other.collide(a)
 
-    def check_collision(self, a1, a2):
-        '''Check two actors for mutual collision
-        (bounding-box collision detection)
-        Args:
-            a1 -- The first actor to check
-            a2 -- The second actor
-        Returns:
-            bool -- Collision value
+    def check_collision(self, a1: Actor, a2: Actor) -> bool:
+        '''Check the two actors (args) for mutual collision (bounding-box
+        collision detection). Return True if colliding, False otherwise
         '''
         x1, y1, w1, h1 = a1.rect()
         x2, y2, w2, h2 = a2.rect()
@@ -94,16 +80,12 @@ class Arena(object):
             and x2 < x1 + w1 and x1 < x2 + w2
             and a1 in self._actors and a2 in self._actors)
 
-    def actors(self):
+    def actors(self) -> list:
         '''Return a copy of the list of actors
-        Returns:
-            list[Actor] -- the registerd actors
         '''
         return list(self._actors)
 
-    def size(self):
-        '''Return the size of the arena as a 2-tuple of ints
-        Returns:
-            (int, int) -- (width, height)
+    def size(self) -> (int, int):
+        '''Return the size of the arena as a 2-tuple of ints: (width, height)
         '''
         return (self._w, self._h)
