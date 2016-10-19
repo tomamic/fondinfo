fcf039c7aa503798a1d2cdb6858a5215ae8069b3
mic <tomamic@ce.unipr.it>
2016-10-19 23:30:18 +0200

    minor
    Former-commit-id: 483a87e7733abc6407e3053d3c357566f51f9929

--- exercises/e5_2011_1_memory.py
+++ exercises/e5_2011_1_memory.py
@@ -7,49 +7,48 @@
 import random
 
 class Memory:
-    JOLLY = '?'
-    FIRST = 'A'
 
     def __init__(self, rows: int, cols: int):
         self._rows, self._cols = rows, cols
-        self._move1 = self._move2 = -1
-        self.sort()
-        self.shuffle()
+        self._pos1 = self._pos2 = -1
+        n = self._rows * self._cols
 
-    def sort(self):
-        # cards: list of ints, representing the cards
-        # visible: list of bools, to keep track of guessed cards
-        self._cards = [i // 2 for i in range(rows * cols)]
-        self._visible = [False] * (rows * cols)
+        self._cards = [chr(i // 2 + ord('A')) for i in range(n)]
+        self._visible = [False for i in range(n)]
 
-    def shuffle(self):
-        random.shuffle(self._cards)
+        # random.shuffle(self._cards)
+        cards = self._cards
+        for i in range(n):
+            j = random.randrange(n)
+            cards[i], cards[j] = cards[j], cards[i]
 
     def uncover(self, pos1: int, pos2: int):
-        if (pos1 != pos2
-            and 0 <= pos1 < len(self._cards)
-            and 0 <= pos2 < len(self._cards)):
-            self._move1, self._move2 = pos1, pos2
-            if self._cards[pos1] == self._cards[pos2]:
+        cards = self._cards
+        n = len(cards)
+        if pos1 != pos2 and 0 <= pos1 < n and 0 <= pos2 < n:
+            self._pos1, self._pos2 = pos1, pos2
+            if cards[pos1] == cards[pos2]:
                 # mark the guessed couple as visible
                 self._visible[pos1] = True
                 self._visible[pos2] = True
 
-    def is_finished(self):
+    def solved(self) -> bool:
         return not (False in self._visible)
 
     def __str__(self) -> str:
         result = []
-        for i, c in enumerate(self._cards):
-            if self._visible[i] or i in (self._move1, self._move2):
-                result.append(chr(c + ord(Memory.FIRST)))
-            else:
-                result.append(Memory.JOLLY)
-            if i % self._cols == self._cols - 1:
-                result.append('\n')
+        for y in range(self._rows):
+            for x in range(self._cols):
+                i = y * self._cols + x
+                if self._visible[i] or i == self._pos1 or i == self._pos2:
+                    result.append(self._cards[i])
+                else:
+                    result.append('?')
+            result.append('\n')
         return ''.join(result)
 
-if __name__ == '__main__':
+
+def main():
     rows = cols = size = 1
     while size % 2 != 0:
         cols = int(input('Cols? '))
@@ -59,8 +58,11 @@ if __name__ == '__main__':
     memory = Memory(rows, cols)
     print(memory)
 
-    while not memory.is_finished():
+    while not memory.solved():
         a = int(input('1st card (1-{})? '.format(size)))
         b = int(input('2nd card (1-{})? '.format(size)))
         memory.uncover(a - 1, b - 1)
         print(memory)
+
+if __name__ == '__main__':
+    main()
