0b8dd07eb4f8e43f654b486601e0de702a712016
mic <tomamic@ce.unipr.it>
2016-12-06 16:54:04 +0100

    dom_knights
    Former-commit-id: 2c19cd2ef2c81d0b43025e7c33c59c16e887c8b2

--- exercises/e5_2016_8_expression.py
+++ exercises/e5_2016_8_expression.py
@@ -9,41 +9,52 @@ class Expression:
         raise NotImplementedError()
     def prefix(self) -> str:
         raise NotImplementedError()
+    def infix(self) -> str:
+        raise NotImplementedError()
 
 
 class Literal:
     def __init__(self, val):
         self._val = val
 
-    def eval(self):
+    def eval(self) -> float:
         return self._val
 
-    def prefix(self):
+    def prefix(self) -> str:
+        return str(self._val)
+
+    def infix(self) -> str:
         return str(self._val)
 
 
 class Product:
-    def __init__(self, op1, op2):
+    def __init__(self, op1: Expression, op2: Expression):
         self._op1 = op1
         self._op2 = op2
 
-    def eval(self):
+    def eval(self) -> float:
         return self._op1.eval() * self._op2.eval()
 
-    def prefix(self):
+    def prefix(self) -> str:
         return "* " + self._op1.prefix() + " " + self._op2.prefix()
+
+    def infix(self) -> str:
+        return self._op1.infix() + " * " + self._op2.infix()
 
 
 class Sum:
-    def __init__(self, op1, op2):
+    def __init__(self, op1: Expression, op2: Expression):
         self._op1 = op1
         self._op2 = op2
 
-    def eval(self):
+    def eval(self) -> float:
         return self._op1.eval() + self._op2.eval()
 
-    def prefix(self):
+    def prefix(self) -> str:
         return "+ " + self._op1.prefix() + " " + self._op2.prefix()
+
+    def infix(self) -> str:
+        return "(" + self._op1.infix() + " + " + self._op2.infix() + ")"
 
 
 def main():
@@ -52,8 +63,8 @@ def main():
     sum1 = Sum(Literal(4), prod1)             # 5   +  (sum1)
     prod2 = Product(Literal(5), sum1)         #    / \
     print(prod2.eval())                       #   4   *  (prod1)
-    print(prod2.prefix())                     #      / \
-                                              #     3   2
+    print(prod2.infix())                      #      / \
+    print(prod2.prefix())                     #     3   2
 
 if __name__ == "__main__":
     main()
