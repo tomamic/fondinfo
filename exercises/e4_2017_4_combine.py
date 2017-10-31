73e9bba4e8b0ac021d22f12838122eada84935e9
tomamic <tomamic@ce.unipr.it>
2017-10-31 09:51:31 +0100

    comprehension
    Former-commit-id: f8df056a49dbea4d109ba15d0c30434ce0b599f9

--- exercises/e4_2017_4_combine.py
+++ exercises/e4_2017_4_combine.py
@@ -2,8 +2,10 @@ def combine(alphabet: str, n: int) -> [str]:
     if n == 0:
         return ['']
 
-    result = []
     combinations = combine(alphabet, n - 1)
+    #return [symbol + comb for symbol in alphabet for comb in combinations]
+
+    result = []
     for symbol in alphabet:
         for comb in combinations:
             result.append(symbol + comb)
