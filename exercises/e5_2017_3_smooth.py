74008e69d79a7b36c32dac3af545e14489460257
tomamic <tomamic@ce.unipr.it>
2017-11-20 23:00:21 +0100

    e5_2017
    Former-commit-id: 14445b246be986c4bea5f70c8c44cab8d4a87da7

---
+++ exercises/e5_2017_3_smooth.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+'''
+@author Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+'''
+
+def avg(matrix: list, x0: int, y0: int) -> int:
+    dirs = ((0, 0), (0, -1), (+1, 0), (0, +1), (-1, 0))
+    rows, cols = len(matrix), len(matrix[0])
+    count, total = 0, 0
+    for dx, dy in dirs:
+        x1, y1 = x0 + dx, y0 + dy
+        if 0 <= x1 < cols and 0 <= y1 < rows:
+            count += 1
+            total += matrix[y1][x1]
+    return total / count
+
+def smooth(matrix: list) -> list:
+    rows, cols = len(matrix), len(matrix[0])
+##    return [[avg(matrix, x, y) for x in range(cols)] for y in range(rows)]
+    result = []
+    for y in range(rows):
+        row = []
+        for x in range(cols):
+            val = avg(matrix, x, y)
+            row.append(val)
+        result.append(row)
+    return result
+            
+def main():
+    matrix = []
+    cols, rows = 0, 0
+
+    with open('matrix.csv', 'r') as file1:
+        for line in file1:
+            splitted = line.split(',')
+            vals = [int(i) for i in splitted]
+            matrix.append(vals)
+            ## matrix += vals  # for a simple list (ex. 4.5)
+            
+            if cols == 0:
+                cols = len(vals)
+            rows += 1
+
+    print(cols, 'x', rows)
+    print(matrix)
+    print()
+    smoothed = smooth(matrix)
+    print(smoothed)
+
+if __name__ == '__main__':
+    main()
---
+++ exercises/e5_2017_5_skyscrapers.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+'''
+@author Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+'''
+
+def count_tops(values: list) -> int:
+    maxx = -1
+    count = 0
+    for val in values:
+        if val > maxx:
+            count += 1
+            maxx = val
+    return count
+
+def transposed(matrix: list) -> list:
+    rows, cols = len(matrix), len(matrix[0])
+##    return [[matrix[y][x] for y in range(rows)] for x in range(cols)]
+
+    result = []
+    for x in range(cols):
+        row = []
+        for y in range(rows):
+            row.append(matrix[y][x])
+        result.append(row)
+    return result
+
+def main():
+    matrix = []
+    cols, rows = 0, 0
+
+    with open('matrix.csv', 'r') as file1:
+        for line in file1:
+            splitted = line.split(',')
+            vals = [int(i) for i in splitted]
+            matrix.append(vals)
+            ## matrix += vals  # for a simple list (ex. 4.5)
+            
+            if cols == 0:
+                cols = len(vals)
+            rows += 1
+
+    print(cols, 'x', rows)
+    print(matrix)
+    print()
+
+    for row in matrix:
+        print(count_tops(row), count_tops(reversed(row)))
+
+    for row in transposed(matrix):
+        print(count_tops(row), count_tops(reversed(row)))
+
+if __name__ == '__main__':
+    main()
