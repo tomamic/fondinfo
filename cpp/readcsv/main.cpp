273a7208e96307203f94a182ddff4f52dcfcd2fc
tomamic <tomamic@ce.unipr.it>
2017-11-20 20:58:14 +0100

    readcsv
    Former-commit-id: 311c539aa7bd5b6b6111789f0c5a88a58649f6b3

--- cpp/readcsv/main.cpp
+++ cpp/readcsv/main.cpp
@@ -21,15 +21,20 @@ int main() {
 
     auto cols = 0, rows = 0;
 
-    vector<int> matrix;
+    vector<int> matrix1;
+//    vector<vector<int>> matrix2;
 
     string line;
     while (getline(file1, line)) {
         auto values = split(line, ',');
-        if (cols == 0) cols = values.size();
-        for (auto v : values) {
-            matrix.push_back(v);
+        if (cols == 0) {
+            cols = values.size();
         }
+
+        for (auto v : values) {
+            matrix1.push_back(v);
+        }
+//        matrix2.push_back(values);
         ++rows;
     }
     file1.close();
@@ -39,7 +44,8 @@ int main() {
     auto total = 0;
     auto x = cols -1, y = rows - 1;
     while (x >= 0 && y >= 0) {
-        total += matrix[y * cols + x];
+        total += matrix1[y * cols + x];
+//        total += matrix2[y][x];
         --x; --y;
     }
     cout << total << endl;
---
+++ cpp/utf8-win/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 65001 > nul");
+    cout << "Oggi è forse il π-day?" << endl;
+    return 0;
+}
