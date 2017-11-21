30f25d75d3b0c2f2fb21967afc734ea222256eb1
tomamic <tomamic@ce.unipr.it>
2017-11-21 12:28:24 +0100

    swig
    Former-commit-id: 11767d144506d8fec989a6e1ecb2e2de06136138

--- cpp/tictactoe/cpp_to_py.sh
+++ cpp/tictactoe/cpp_to_py.sh
@@ -3,5 +3,5 @@
 ## sudo apt install swig python3-dev
 
 swig -python -c++ tictactoe.i
-g++ -fPIC -c tictactoe.cpp tictactoe_wrap.cxx -I/usr/include/python3.6m/ -std=c++11
-g++ -shared tictactoe.o tictactoe_wrap.o -o _tictactoe.so
+gcc -fPIC -c tictactoe.cpp tictactoe_wrap.cxx -I/usr/include/python3.6m/ -std=c++11
+gcc -shared tictactoe.o tictactoe_wrap.o -o _tictactoe.so
