59a11cba8fbdf6f18129540cec124d7e73706cbe
tomamic <tomamic@ce.unipr.it>
2018-02-23 19:10:23 +0100

    swig
    Former-commit-id: 3c7e05a0fd843af8825008bad6b3b641b29f20d4

---
+++ cpp/boardgame/cpp2py.bat
@@ -0,0 +1,12 @@
+set MODULE=knightdom
+
+set SWIG_HOME=%USERPROFILE%\Downloads\swig-win-3.0.12\swig-win-3.0.12
+set PYTHON_HOME=%LOCALAPPDATA%\Programs\Python\Python36-32
+set MINGW_HOME=%PROGRAMFILES(X86)%\CodeBlocks\MinGW
+
+"%SWIG_HOME%\swig.exe" -python -c++ %MODULE%.i
+set SAVED_PATH=%PATH%
+set PATH=%MINGW_HOME%\bin
+g++ -std=c++11 -D_hypot=hypot -c %MODULE%.cpp %MODULE%_wrap.cxx -I"%PYTHON_HOME%\include"
+g++ -shared *.o "%PYTHON_HOME%\libs\libpython36.a" -o _%MODULE%.pyd
+set PATH=%SAVED_PATH%
