6636b40ad4b28b7820632d6149b59edd4dd52e1e
tomamic <tomamic@ce.unipr.it>
2018-02-28 11:26:48 +0100

    c++14
    Former-commit-id: 740d6e86b4e11e466cdd8825acd148285ae1f211

--- cpp/cpp2py.bat
+++ cpp/cpp2py.bat
@@ -11,6 +11,7 @@ for %%f in (*.i) do (
     swig -python -c++ !MODULE!.i
     g++ -std=c++14 -D_hypot=hypot -c *.cpp *.cxx -I"%PYTHON_HOME%\include"
     g++ -shared *.o "%PYTHON_HOME%\libs\libpython36.a" -o _!MODULE!.pyd
+    del *.o *wrap.cxx
 )
 
 pause
