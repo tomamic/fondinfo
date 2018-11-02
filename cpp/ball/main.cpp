aa8a8d1652bae3547ee9889b6521ef7f9c4eccf3
tomamic <tomamic@ce.unipr.it>
2018-11-03 00:18:58 +0100

    minor

--- cpp/ball/main.cpp
+++ cpp/ball/main.cpp
@@ -8,15 +8,15 @@ int main()
     Ball ball1{40, 80};
     auto ball2 = new Ball{80, 40};
 
-    string line;
-    while (getline(cin, line)) {
+
+    for (auto i = 0; i < 25; ++i) {
         ball1.move();
         ball2->move();
 
         cout << ball1.get_x() << ", " << ball1.get_y() << endl;
         cout << ball2->get_x() << ", " << ball2->get_y() << endl << endl;
     }
-    
+
     delete ball2;
     return 0;
 }
