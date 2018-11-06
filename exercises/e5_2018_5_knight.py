02262d3b2d5478cec9028ad7be13223b68a85f71
tomamic <tomamic@ce.unipr.it>
2018-11-06 14:39:17 +0100

    knight

--- exercises/e5_2018_5_knight.py
+++ exercises/e5_2018_5_knight.py
@@ -5,8 +5,8 @@ def print_matrix(m: list, w: int, h: int):
         print()
 
 def main():
-    w = int(input("w? ")
-    h = int(input("h? ")
+    w = int(input("w? "))
+    h = int(input("h? "))
     n, count = w * h, 1
     last_x, last_y = 0, 0
     matrix = [0] * n
@@ -14,13 +14,14 @@ def main():
 
     while count < n:
         print_matrix(matrix, w, h)
-        x = int(input("x? ")
-        y = int(input("y? ")
+        x = int(input("x? "))
+        y = int(input("y? "))
         if 0 <= x < w and 0 <= y < h:
             dx, dy = abs(x - last_x), abs(y - last_y)
             if matrix[y * w + x] == 0 and (dx, dy) in [(2, 1), (1, 2)]:
                 count += 1
                 matrix[y * w + x] = count
                 last_x, last_y = x, y
+    print_matrix(matrix, w, h)
 
 main()
