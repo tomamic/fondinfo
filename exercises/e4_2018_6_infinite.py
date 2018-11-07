aefbc74e8f369c632452d8bb0c3590a136b2b730
tomamic <tomamic@ce.unipr.it>
2018-11-07 19:23:42 +0100

    infinite

---
+++ exercises/e4_2018_6_infinite.py
@@ -0,0 +1,15 @@
+import g2d
+
+def infinite_circles(x: int, y: int, w: int, h: int,
+                     c1: (int, int, int), c2: (int, int, int)):
+    w2, h2 = w // 2, h // 2
+    if h2 < 1:
+        return
+    g2d.draw_circle(c1, (x + w2, y + h2), h2)
+
+    infinite_circles(x, y, w, h2, c2, c1)
+    infinite_circles(x, y + h2, w, h2, c2, c1)
+
+g2d.init_canvas((600, 600))
+infinite_circles(10, 10, 580, 580, (0, 0, 255), (255, 255, 0))
+g2d.main_loop()
