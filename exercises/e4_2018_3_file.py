ef057208ec16f4385693d48723e32b004941b7c0
tomamic <tomamic@ce.unipr.it>
2018-11-07 18:47:19 +0100

    exercises

---
+++ exercises/e4_2018_3_file.py
@@ -0,0 +1,22 @@
+def generate_file():
+    from random import uniform
+    with open("values.csv", "w") as csv_out:
+        for y in range(20):
+            print(uniform(0, 50), uniform(0, 50), sep=",", file=csv_out)
+
+def main():
+    max_val, min_val = 0, 0
+    total, count = 0, 0
+    with open("values.csv", "r") as csv:
+        for line in csv:
+            for word in line.split(","):
+                val = float(word)
+                if count == 0 or val > max_val:
+                    max_val = val
+                if count == 0 or val < min_val:
+                    min_val = val
+                total += val
+                count += 1
+
+    if count > 0:
+        print(max_val, min_val, total / count)
