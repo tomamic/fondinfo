0827e135838793fac94d4c717b819a6963aeb6f2
tomamic <tomamic@ce.unipr.it>
2018-12-12 18:11:23 +0100

    knight

---
+++ exercises/e4_2014_9_knight_tour.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+'''
+@author Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+'''
+
+import sys
+sys.setrecursionlimit(100000)
+
+
+delta = [(+1, -2), (+2, -1), (+2, +1), (+1, +2),
+         (-1, +2), (-2, +1), (-2, -1), (-1, -2)]
+
+
+def find_moves(board, x0, y0) -> list:
+    w, h = len(board[0]), len(board)
+    moves = []
+    for dx, dy in delta:
+        x, y = x0 + dx, y0 + dy
+        if (0 <= x < w and 0 <= y < h and board[y][x] == 0):
+            moves.append((x, y))
+    return moves
+
+
+def border_distance(x, y, w, h):
+    dist_x = min(x, w - x)
+    dist_y = min(y, h - y)
+##    return dist_x + dist_y
+    return sorted((dist_x, dist_y))
+
+
+def knight_tour(board, x, y) -> bool:
+    w, h = len(board[0]), len(board)
+    n = board[y][x]
+    if n == w * h:
+        return True
+
+    moves = find_moves(board, x, y)
+##    moves.sort(key=lambda m: border_distance(m[0], m[1], w, h))
+##    moves.sort(key=lambda m: len(find_moves(board, m[0], m[1])))
+
+    # try each possible move (recursively)
+    for xm, ym in moves:
+        board[ym][xm] = n + 1
+
+        if knight_tour(board, xm, ym):
+            return True
+
+        # no luck this way, go back (backtracking)
+        board[ym][xm] = 0
+    return False
+
+
+def fast_tour(board, x, y) -> bool:
+    w, h = len(board[0]), len(board)
+    n = board[y][x]
+    while n < w * h:
+        moves = find_moves(board, x, y)
+        if len(moves) == 0:
+            return False
+        n += 1
+##        x, y = min(moves, key=lambda m: border_distance(m[0], m[1], w, h))
+        x, y = min(moves, key=lambda m: len(find_moves(board, m[0], m[1])))
+        board[y][x] = n
+    return True
+
+
+def main():
+    #w = h = int(input('size? '))
+    for s in range(5, 100):
+        w = h = s
+        board = [[0 for x in range(w)] for y in range(h)]
+        board[0][0] = 1
+
+        print(w, h, knight_tour(board, 0, 0))
+
+        if max(w, h) <= 8:
+            for y in range(h):
+                for x in range(w):
+                    print(("{:4}").format(board[y][x]), end='')
+                print()
+
+main()
