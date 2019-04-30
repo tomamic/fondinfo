36226492aba71830eae48b0eea5b27ae88b107fe
tomamic <tomamic@ce.unipr.it>
2019-04-30 15:24:01 +0200

    run

---
+++ cpp/run.bat
@@ -0,0 +1,5 @@
+if not x%PATH:MinGW=%==x%PATH% setx PATH %PATH%;C:\MinGW\mingw64\bin
+
+del _main.exe
+g++ *.cpp -std=c++14 -DWEBVIEW_WINAPI=1 -lole32 -lcomctl32 -loleaut32 -luuid -mwindows -o _main.exe
+_main.exe
---
+++ cpp/run.sh
@@ -0,0 +1,2 @@
+g++ main.cpp -DWEBVIEW_GTK=1 `pkg-config --cflags --libs gtk+-3.0 webkit2gtk-4.0` -o _main
+./_main
