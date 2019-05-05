00b0b65812652ba08cb2f80eccd98bcdbc6853bc
tomamic <tomamic@ce.unipr.it>
2019-05-05 16:02:10 +0200

    cpath

--- cpp/setpath.py
+++ cpp/setpath.py
@@ -1,8 +1,10 @@
 import os
-folders = ["C:\\MinGw\mingw64\\bin"]
+folders = ["C:\\MinGW\\bin", "C:\\MinGW\\mingw64\\bin"]
 for elem in os.environ["PATH"].split(";"):
 	if elem and elem not in folders:
 		folders.append(elem)
 newpath = ";".join(folders)
 os.environ["PATH"] = newpath
 os.system(f'setx PATH "{newpath}"')
+os.system(f'setx CPATH "C:\\MinGW\\include"')
+os.system(f'setx LD_LIBRARY_PATH "C:\\MinGW\\lib"')
