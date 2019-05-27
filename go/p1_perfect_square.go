d8064ff252decb8f062bf1c4d0f13752bd3f4759
tomamic <tomamic@ce.unipr.it>
2019-05-27 22:47:26 +0200

    exercises

--- go/p1_perfect_square.go
+++ go/p1_perfect_square.go
@@ -3,20 +3,15 @@ package main
 import . "g2d"
 
 func main() {
-	n := ToInt(Prompt("n? "))
-	result := 0
+    n := ToInt(Prompt("n?"))
 
-	i := 1
-	for i*i <= n {
-		if i*i == n {
-			result = i
-		}
-		i += 1
-	}
-
-	if result == 0 {
-		Alert("No")
-	} else {
-		Alert(result)
-	}
+    i := 1
+    for i*i < n {
+        i++
+    }
+    if i*i == n {
+        Alert(i)
+    } else {
+        Alert("No")
+    }
 }
