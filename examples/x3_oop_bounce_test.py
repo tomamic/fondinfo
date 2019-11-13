98cc19e7e584df2cc0bf6b34508d4cea414279a9
tomamic <tomamic@ce.unipr.it>
2019-11-13 21:50:10 +0100

    test

---
+++ examples/x3_oop_bounce_test.py
@@ -0,0 +1,56 @@
+import unittest
+from p3_oop_bounce import Arena, Ball, Ghost, Turtle
+
+
+class BallTest(unittest.TestCase):
+
+    def setUp(self):
+        self.a = Arena((480, 360))
+
+    def test_corner(self):
+        b = Ball(self.a, (460, 340))  # dx = 5, dy = 5
+        b.move()  # dx = -5, dy = -5
+        b.move()
+        self.assertTrue(b.position() == (450, 330, 20, 20))
+
+    def test_move(self):
+        test_values = ( (40, 80, 45, 85),
+                        (40, 215, 45, 220),
+                        (40, 340, 45, 335),
+                        (295, 80, 300, 85),
+                        (460, 80, 455, 85) )
+        for param in test_values:
+            x0, y0, x1, y1 = param
+            b = Ball(self.a, (x0, y0))
+            b.move()
+            self.assertTrue(b.position() == (x1, y1, 20, 20))
+
+
+class TurtleTest(unittest.TestCase):
+
+    def setUp(self):
+        self.a = Arena((480, 360))
+
+    def test_right(self):
+        t = Turtle(self.a, (230, 170))
+        t.go_right(True)
+        t.move()
+        t.move()
+        t.go_right(False)
+        t.move()  # no effect
+        self.assertTrue(t.position() == (234, 170, 20, 20))
+
+    def test_collide_ball(self):
+        t = Turtle(self.a, (230, 170))
+        t.collide(Ball(self.a, (0, 0)))
+        t.collide(Ball(self.a, (0, 0)))  # no effect
+        self.assertTrue(t.lives() == 2)
+
+    def test_collide_ghost(self):
+        t = Turtle(self.a, (230, 170))
+        t.collide(Ghost(self.a, (0, 0)))
+        self.assertTrue(t.lives() == 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
