9ec703973c807f9a8c7b193f2b1686b6a22d0b31
tomamic <tomamic@ce.unipr.it>
2020-09-28 23:34:48 +0200

    minor

--- examples/p1_sentinel.py
+++ examples/p1_sentinel.py
@@ -7,11 +7,11 @@
 count = 0
 total = 0
 
-val = int(input("Val? [0 to end] "))
-while val != 0:
+val = int(input("Val? [-1 to end] "))
+while val != -1:
     total += val  # total = total + val
     count += 1    # count = count + 1
-    val = int(input("Val? [0 to end] "))
+    val = int(input("Val? [-1 to end] "))
 
 if count != 0:
     print("The average is", total / count)
