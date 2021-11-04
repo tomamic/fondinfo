4e4cc4620764b1465873761db305b362bde2fe0e
tomamic <tomamic@ce.unipr.it>
2021-11-04 19:09:43 +0100

    htree

--- examples/x4_fun_htree.py
+++ examples/x4_fun_htree.py
@@ -6,30 +6,25 @@
 
 import g2d
 
-def center(pos: (int, int), size: (int, int)) -> (int, int):
-    x, y, w, h = pos + size
-    return x + w / 2, y + h / 2
-
 def htree(pos: (int, int), size: (int, int), level: int):
     x, y, w, h = pos + size
-    if level == 0 or w < 3 or h < 3:
+    if level == 0 or w < 10 or h < 10:
         return
-    if level % 2 == 0:
-        pos2 = x + w / 2, y
-        size2 = w / 2, h
-    else:
-        pos2 = x, y + h / 2
-        size2 = w, h / 2
 
-    g2d.draw_line(center(pos, size2), center(pos2, size2))
-    htree(pos, size2, level - 1)
-    htree(pos2, size2, level - 1)
+    x1, x2, x3 = x + 1*w//4, x + 2*w//4, x + 3*w//4
+    y1, y2, y3 = y + 1*h//4, y + 2*h//4, y + 3*h//4
+    g2d.draw_line((x1, y1), (x1, y3))
+    g2d.draw_line((x3, y1), (x3, y3))
+    g2d.draw_line((x1, y2), (x3, y2))
 
-side = 600
+    for pt in ((x, y), (x2, y), (x, y2), (x2, y2)):
+        htree(pt, (w//2, h//2), level-1)
 
-g2d.init_canvas((side, side))
+def main():
+    size = 600, 600
+    g2d.init_canvas(size)
+    level = int(g2d.prompt('level? '))  ## -1 = infinite
+    htree((0, 0), size, level)
+    g2d.main_loop()
 
-level = int(g2d.prompt('level? '))  ## -1 = infinite
-htree((0, 0), (side, side), level)
-
-g2d.main_loop()
+main()
