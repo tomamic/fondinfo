c27fb168171933add6fabddb12012dd006c56a81
tomamic <tomamic@ce.unipr.it>
2021-11-04 12:53:05 +0100

    no_rect

--- examples/x4_fun_htree.py
+++ examples/x4_fun_htree.py
@@ -6,30 +6,30 @@
 
 import g2d
 
-def center(rect: (int, int, int, int)) -> (int, int):
-    x, y, w, h = rect
+def center(pos: (int, int), size: (int, int)) -> (int, int):
+    x, y, w, h = pos + size
     return x + w / 2, y + h / 2
 
-def htree(rect: (int, int, int, int), level: int):
-    x, y, w, h = rect
+def htree(pos: (int, int), size: (int, int), level: int):
+    x, y, w, h = pos + size
     if level == 0 or w < 3 or h < 3:
         return
     if level % 2 == 0:
-        rect1 = x, y, w / 2, h
-        rect2 = x + w / 2, y, w / 2, h
+        pos2 = x + w / 2, y
+        size2 = w / 2, h
     else:
-        rect1 = x, y, w, h / 2
-        rect2 = x, y + h / 2, w, h / 2
+        pos2 = x, y + h / 2
+        size2 = w, h / 2
 
-    g2d.draw_line(center(rect1), center(rect2))
-    htree(rect1, level - 1)
-    htree(rect2, level - 1)
+    g2d.draw_line(center(pos, size2), center(pos2, size2))
+    htree(pos, size2, level - 1)
+    htree(pos2, size2, level - 1)
 
 side = 600
 
 g2d.init_canvas((side, side))
 
 level = int(g2d.prompt('level? '))  ## -1 = infinite
-htree((0, 0, side, side), level)
+htree((0, 0), (side, side), level)
 
 g2d.main_loop()
