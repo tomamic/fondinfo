9a4881b82627987b51ea384c91b133d02131029f
tomamic <tomamic@ce.unipr.it>
2022-02-03 10:57:35 +0100

    randpt

--- examples/x3_oop_bounce_game.py
+++ examples/x3_oop_bounce_game.py
@@ -4,24 +4,35 @@
 @license This software is free - http://www.gnu.org/licenses/gpl.html
 '''
 
+from random import randrange
 from p3_oop_bounce import Actor, Arena, Ball, Ghost, Turtle
 
+def randpt(sz) -> (int, int):
+    w, h, xc, yc = sz[0], sz[1], sz[0] // 2, sz[1] // 2
+    x, y = randrange(w), randrange(h)
+    while (x - xc) ** 2 + (y - yc) ** 2 < 100 ** 2:
+        x, y = randrange(w), randrange(h)
+    return x, y
 
 class BounceGame:
-    def __init__(self):
-        self._arena = Arena((480, 360))
-        self._arena.spawn(Ball((40, 80)))
-        self._arena.spawn(Ball((80, 40)))
-        '''from random import randrange
-        for _ in range(500):
-            self._arena.spawn(Ball((randrange(480), randrange(360))))'''
-        self._arena.spawn(Ghost((120, 80)))
-        self._arena.spawn(Turtle((80, 80)))
-        self._time = 120 * 30  # 120 seconds
+    def __init__(self, size=(480, 360), nballs=3, nghosts=2, secs=120):
+        self._arena = Arena(size)
+        sz = size[0] - 20, size[1] - 20
+        self._arena.spawn(Turtle((sz[0] // 2, sz[1] // 2)))  # center
+        for _ in range(nballs):
+            self._arena.spawn(Ball(randpt(sz)))
+        for _ in range(nghosts):
+            self._arena.spawn(Ghost(randpt(sz)))
+        self._time = secs * 30  # 120 seconds
+        self._lives = 1
 
     def tick(self, keys=tuple()):
         self._arena.tick(keys)
         self._time -= 1
+        self._lives = 0
+        for a in self._arena.actors():
+            if isinstance(a, Turtle):
+                self._lives = a.lives()
 
     def size(self) -> (int, int):
         return self._arena.size()
@@ -30,17 +41,13 @@ class BounceGame:
         return self._arena.actors()
 
     def game_over(self) -> bool:
-        return self.lives() <= 0
+        return self._lives <= 0
 
     def game_won(self) -> bool:
         return self._time <= 0
 
     def lives(self) -> int:
-        lives = 0
-        for a in self._arena.actors():
-            if isinstance(a, Turtle):
-                lives = a.lives()
-        return lives
+        return self._lives
 
     def time(self) -> int:
         return self._time // 30
