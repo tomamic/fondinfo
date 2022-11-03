305fd687a80ecafd26c695f4606f96e4c229369b
tomamic <tomamic@ce.unipr.it>
2022-11-03 09:32:04 +0100

    gcd

--- p08_gcd.py
+++ p08_gcd.py
@@ -4,27 +4,21 @@
 @license This software is free - http://www.gnu.org/licenses/gpl.html
 '''
 
-import logging
-
-def gcd1(a: int, b: int) -> int:
-    logging.debug(f"gcd1 {a} {b}")
+def gcd(a: int, b: int) -> int:
     if b == 0:
         return a
-    return gcd1(b, a % b)
+    return gcd(b, a % b)
 
-def gcd2(a: int, b: int) -> int:
-    logging.debug(f"gcd2 {a} {b}")
+def gcd_it(a: int, b: int) -> int:
     while b != 0:
         a, b = b, a % b
-        logging.debug(f"gcd2 {a} {b}")
     return a
 
 def main():
-##    logging.basicConfig(level=logging.DEBUG)
-##    a, b = 1071, 1029
-    a = int(input("a? "))
-    b = int(input("b? "))
-    print(gcd1(a, b))
-    print(gcd2(a, b))
+    a, b = 1071, 1029
+    # a = int(input("a? "))
+    # b = int(input("b? "))
+    result = gcd(a, b)
+    print(result)
 
 main()
