9ea8f5bcce497c1a4d5712099c1148d7df485a5c
tomamic <tomamic@ce.unipr.it>
2022-11-03 10:18:19 +0100

    logging

--- p08_fibonacci.py
+++ p08_fibonacci.py
@@ -4,13 +4,12 @@
 @license This software is free - http://www.gnu.org/licenses/gpl.html
 '''
 
-import logging, time, functools
+import functools, time
 
 @functools.lru_cache()
 def fibonacci1(n: int) -> int:
     if n < 2:
         return n
-    logging.debug("fib " + str(n))
     return fibonacci1(n - 1) + fibonacci1(n - 2)
 
 _fibonacci2_lookup = [0, 1]
@@ -18,23 +17,18 @@ _fibonacci2_lookup = [0, 1]
 def fibonacci2(n: int) -> int:
     if n < len(_fibonacci2_lookup):
         return _fibonacci2_lookup[n]
-    logging.debug("fib " + str(n))
     result = fibonacci2(n - 1) + fibonacci2(n - 2)
     _fibonacci2_lookup.append(result)
     return result
 
 def fibonacci3(n: int) -> int:
     val, nxt = 0, 1
-
     for i in range(n):
-        logging.debug("fib " + str(n))
         val, nxt = nxt, val + nxt
-
     return val
 
 
 def main():
-##    logging.basicConfig(level=logging.DEBUG)
     n = int(input("n? "))
 
     start = time.time()
