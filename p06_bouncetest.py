c076c9bc0227edd6fe48fa1100c9363bd05f16c1
tomamic <tomamic@ce.unipr.it>
2022-11-05 23:58:12 +0100

    mock

--- p06_bouncetest.py
+++ p06_bouncetest.py
@@ -1,18 +1,21 @@
-import unittest
+from unittest import TestCase, main
+from unittest.mock import Mock
 from p05_bouncegame import Arena, Ball, Ghost, TurtleHero
 
 
-class BallTest(unittest.TestCase):
+class BallTest(TestCase):
 
     def test_corner(self):
-        a = Arena((480, 360))
-        b = Ball((460, 340))  # dx = 4, dy = 4
-        b.move(a)  # dx = -4, dy = -4
-        b.move(a)
-        self.assertEqual(b.pos(), (452, 332))
+        arena = Mock()
+        arena.size.return_value = (480, 360)
+        ball = Ball((460, 340))  # dx = 4, dy = 4
+        ball.move(arena)  # dx = -4, dy = -4
+        ball.move(arena)
+        self.assertEqual(ball.pos(), (452, 332))
 
     def test_move(self):
-        a = Arena((480, 360))
+        arena = Mock()
+        arena.size.return_value = (480, 360)
         test_values = ( (40, 80, 44, 84),
                         (40, 215, 44, 219),
                         (40, 340, 44, 336),
@@ -20,38 +23,41 @@ class BallTest(unittest.TestCase):
                         (460, 80, 456, 84) )
         for param in test_values:
             x0, y0, x1, y1 = param
-            b = Ball((x0, y0))
-            b.move(a)
-            self.assertEqual(b.pos(), (x1, y1))
+            ball = Ball((x0, y0))
+            ball.move(arena)
+            self.assertEqual(ball.pos(), (x1, y1))
 
 
-class TurtleTest(unittest.TestCase):
+class TurtleTest(TestCase):
 
     def test_right(self):
-        a = Arena((480, 360))
-        a.tick(("ArrowRight"))
-        t = TurtleHero((230, 170))
-        t.move(a)
-        t.move(a)
-        a.tick()
-        t.move(a)  # no effect
-        self.assertEqual(t.pos(), (234, 170))
+        arena = Mock()
+        arena.size.return_value = (480, 360)
+        arena.current_keys.return_value = ["ArrowRight"]
+        turtle = TurtleHero((230, 170))
+        turtle.move(arena)
+        turtle.move(arena)
+        arena.current_keys.return_value = []
+        turtle.move(arena)  # no effect
+        self.assertEqual(turtle.pos(), (234, 170))
 
     def test_collide_ball(self):
-        a = Arena((480, 360))
-        b = Ball((0, 0))
-        t = TurtleHero((230, 170))
-        t.collide(b, a)
-        t.collide(b, a)  # no effect
-        self.assertEqual(t.lives(), 2)
+        arena = Mock()
+        arena.size.return_value = (480, 360)
+        ball = Mock(spec=Ball)  # to fool isinstance
+        turtle = TurtleHero((230, 170))
+        turtle.collide(ball, arena)
+        turtle.collide(ball, arena)  # no effect
+        self.assertEqual(turtle.lives(), 2)
 
     def test_collide_ghost(self):
-        a = Arena((480, 360))
-        g = Ghost((0, 0))
-        t = TurtleHero((230, 170))
-        t.collide(g, a)
-        self.assertEqual(t.lives(), 3)
+        arena = Mock()
+        arena.size.return_value = (480, 360)
+        ghost = Mock(spec=Ghost)  # to fool isinstance
+        turtle = TurtleHero((230, 170))
+        turtle.collide(ghost, arena)
+        self.assertEqual(turtle.lives(), 3)
 
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
