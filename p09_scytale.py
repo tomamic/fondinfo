2383468ad91cf0be7a1a2e920fc70dff5660247f
tomamic <tomamic@ce.unipr.it>
2022-11-08 15:18:52 +0100

    list

--- p09_scytale.py
+++ p09_scytale.py
@@ -4,26 +4,26 @@
 @license This software is free - http://www.gnu.org/licenses/gpl.html
 '''
 
-text = ""
-with open("p5_mat_scytale.py") as infile:
-    text = infile.read()
+text = "informazioneSEGRETISSIMA"
+#with open("p09_scytale.py") as infile:
+#    text = infile.read()
 
 with open("_output.txt", "w") as outfile:
     FILLER = ' '
     ROWS = 3
     COLS = 4
 
-    matrix = [[' '] * COLS for y in range(ROWS)]
+    matrix = [FILLER] * (ROWS * COLS)
     i = 0
     while i < len(text):
         for y in range(ROWS):
             for x in range(COLS):
                 if i < len(text):
-                    matrix[y][x] = text[i]
+                    matrix[y * COLS + x] = text[i]
                     i += 1
                 else:
-                    matrix[y][x] = FILLER
+                    matrix[y * COLS + x] = FILLER
 
         for x in range(COLS):
             for y in range(ROWS):
-                print(matrix[y][x], end="", file=outfile)
+                print(matrix[y * COLS + x], end="", file=outfile)
--- p09_smooth.py
+++ p09_smooth.py
@@ -4,7 +4,7 @@
 @license This software is free - http://www.gnu.org/licenses/gpl.html
 '''
 
-def avg(matrix: list, rows: int, cols: int, x0: int, y0: int) -> int:
+def avg(matrix: list, cols: int, rows: int, x0: int, y0: int) -> float:
     dirs = ((0, 0), (0, -1), (+1, 0), (0, +1), (-1, 0))
     count, total = 0, 0
     for dx, dy in dirs:
@@ -14,20 +14,19 @@ def avg(matrix: list, rows: int, cols: int, x0: int, y0: int) -> int:
             total += matrix[y1 * cols + x1]
     return total / count
 
-def smooth(matrix: list, rows: int, cols: int) -> list:
-##    return [avg(matrix, rows, cols, x, y) for y in range(rows) for x in range(cols)]
+def smooth(matrix: list[float], cols: int, rows: int) -> list[float]:
+##    return [avg(matrix, cols, rows, x, y) for y in range(rows) for x in range(cols)]
     result = []
     for y in range(rows):
         for x in range(cols):
-            val = avg(matrix, rows, cols, x, y)
+            val = avg(matrix, cols, rows, x, y)
             result.append(val)
     return result
 
 def main():
     matrix = []
     cols, rows = 0, 0
-
-    with open('matrix.csv', 'r') as file1:
+    with open('_matrix.csv', 'r') as file1:
         for line in file1:
             splitted = line.split(',')
             vals = [int(i) for i in splitted]
@@ -38,7 +37,7 @@ def main():
     print(cols, 'x', rows)
     print(matrix)
     print()
-    smoothed = smooth(matrix, rows, cols)
+    smoothed = smooth(matrix, cols, rows)
     print(smoothed)
 
 if __name__ == '__main__':
