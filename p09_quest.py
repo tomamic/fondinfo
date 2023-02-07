2cf22d957562932c67a65469896281f0fc9aa612
tomamic <tomamic@ce.unipr.it>
2023-02-07 12:29:07 +0100

    quest

--- p09_quest.py
+++ p09_quest.py
@@ -7,35 +7,35 @@
 from boardgame import BoardGame
 from random import sample
 
-FREE, ORC, GOLD, PIKD, OUT = range(5)
+FREE, ORC, GOLD, FOUND, OUT = range(5)
 
 class Quest(BoardGame):
     def __init__(self, w: int, h: int, golds: int, orcs: int):
         if (free_cells := w * h - golds - orcs - 1) < 0:
             raise VaueError("Too many golds and orcs")
         bd = [GOLD] * golds + [ORC] * orcs + [FREE] * free_cells
-        self._bd = [FREE] + sample(bd, len(bd))  # shuffled(bd)
+        self._bd = [FREE] + sample(bd, len(bd))  # shuffled bd
         self._x = self._y = 0
         self._w, self._h = w, h
         self._golds, self._lost = golds, False
 
     def _get(self, x, y) -> int:
         bd, w, h = self._bd, self._w, self._h
-        return bd[y * w + x] if 0 <= x < w and 0 <= y < h else OUT
+        return bd[y * w + x] if (0 <= x < w and 0 <= y < h) else OUT
 
     def play(self, x: int, y: int, action: str):
         v = self._get(x, y)
         if v != OUT and abs(x - self._x) + abs(y - self._y) == 1:
             if v == GOLD:
-                self._bd[y * self._w + x] = PIKD
+                self._bd[y * self._w + x] = FOUND
                 self._golds -= 1
             self._lost = v == ORC
             self._x, self._y = x, y
 
     def read(self, x: int, y: int) -> str:
-        v = self._get(x, y)
-        return ("\U0001F479" if v==ORC and self.finished() else
-                "\U0001F4B0" if v==PIKD or v==GOLD and self._lost else
+        v, fin = self._get(x, y), self.finished()
+        return ("\U0001F479" if v == ORC and fin else
+                "\U0001F4B0" if v == FOUND or v == GOLD and fin else
                 "\U0001F934" if (x, y) == (self._x, self._y) else "")
 
     def finished(self) -> bool:
