24ed0052fd144d8593746d1635abb4efb1cf097d
tomamic <tomamic@ce.unipr.it>
2023-02-21 21:17:21 +0100

    dnd

--- p04_dnd.py
+++ p04_dnd.py
@@ -6,36 +6,36 @@
 
 from random import randint
 
-class Character:
+class Fighter:
     def __init__(self, name: str):
         self._name = name
         self._hp = randint(15, 30)  # hit points
 
     def hit(self, damage: int) -> None:
         self._hp = max(self._hp - damage, 0)
-        
+
     def heal(self, cure: int) -> None:
         if (self.alive()):
             self._hp = min(self._hp + cure, 20)
-        
+
     def alive(self) -> bool:
         return self._hp > 0
-    
+
     def describe(self) -> str:
         return f"I'm {self._name}. I have {self._hp} hit points."
-    
+
 
 def main():
-    c = Character("Hero")
+    c = Fighter("Hero")
     print(c.describe())
 
     for _ in range(3):
         c.hit(randint(5, 10))
         print(c.describe())
-    
+
     c.heal(randint(5, 10))
     print(c.describe())
-    
+
     print(c.alive())
 
 
