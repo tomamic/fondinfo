36b5545daa3ad1aa70ddb3a8ece1fab2d5efefba
tomamic <tomamic@ce.unipr.it>
2023-03-01 23:18:17 +0100

    dnd

--- p04_dnd.py
+++ p04_dnd.py
@@ -16,7 +16,7 @@ class Fighter:
 
     def heal(self, cure: int) -> None:
         if (self.alive()):
-            self._hp = min(self._hp + cure, 20)
+            self._hp = min(self._hp + cure, 30)
 
     def alive(self) -> bool:
         return self._hp > 0
