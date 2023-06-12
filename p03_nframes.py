fe6cc8b0aad1b8ff8443bda769d15d8fe1f6c1eb
tomamic <tomamic@ce.unipr.it>
2023-06-12 21:00:27 +0200

    vertical

--- p03_nframes.py
+++ p03_nframes.py
@@ -6,22 +6,22 @@
 
 import g2d
 
-x, y, dx, count = 40, 40, -4, 0
+x, y, dy, count = 40, 40, -4, 0
 ARENA_W, ARENA_H = 480, 360
 
 def tick():
-    global x, dx, count
+    global y, dy, count
     g2d.clear_canvas()
     g2d.draw_image("ball.png", (x, y))
     if g2d.mouse_clicked() and count == 0:
         count = 5
-        dx = -dx
+        dy = -dy
     if count > 0:
         count -= 1
-        x += dx
+        y += dy
 
 def main():
     g2d.init_canvas((ARENA_W, ARENA_H))
-    g2d.main_loop(tick, 5)  # call tick 5 times/second
+    g2d.main_loop(tick, 10)  # call tick 10 times/second
 
 main()
