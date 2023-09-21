ad40d98a04f905ef7a4149cef46aa19283fc960e
tomamic <tomamic@ce.unipr.it>
2023-09-21 10:16:46 +0200

    slope

--- p01_bluerow.py
+++ p01_bluerow.py
@@ -11,13 +11,13 @@ g2d.init_canvas((L, L))
 
 n = int(g2d.prompt("N?"))
 m_blue = 255 / max(n - 1, 1)
-m_pos = L / max(n, 1)
-r = d / 2
+diameter = L / max(n, 1)
+radius = diameter / 2
 
 for i in range(n):
-    x = r + i * m_pos
+    x = radius + i * diameter
     blue = i * m_blue
     g2d.set_color((0, 0, blue))
-    g2d.draw_circle((x, r), r)
+    g2d.draw_circle((x, radius), radius)
 
 g2d.main_loop()
