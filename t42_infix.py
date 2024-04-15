11be5cfd93e8786c3b6129b5f6bd80a884fe9250
tomamic <tomamic@ce.unipr.it>
2024-04-15 22:23:43 +0200

    parse_infix

--- t42_infix.py
+++ t42_infix.py
@@ -9,70 +9,6 @@ from math import isclose
 from operator import add, sub, mul, truediv, neg
 ops = {"+": add, "-": sub, "*": mul, "/": truediv, "~": neg}
 
-# expr = term {( "+" | "-" ) term}
-# term = factor {( "*" | "/" ) factor}
-# factor = "-" factor | "(" expr ")" | identifier | number
-# (identifiers start with a letter, numbers are float)
-
-# expr = term {( "+" | "-" ) term}
-def expr(tok: "Tokenizer") -> "Expr":
-    x = term(tok)
-    while tok.peek() in ("+", "-"):
-        op = tok.peek()
-        tok.consume(op)
-        y = term(tok)
-        x = BinaryOp(op, x, y)
-    return x
-
-# term = factor {( "*" | "/" ) factor}
-def term(tok: "Tokenizer") -> "Expr":
-    x = factor(tok)
-    while tok.peek() in ("*", "/"):
-        op = tok.peek()
-        tok.consume(op)
-        y = factor(tok)
-        x = BinaryOp(op, x, y)
-    return x
-
-# factor = "-" factor | "(" expr ")" | identifier | number
-def factor(tok: "Tokenizer") -> "Expr":
-    nxt = tok.peek()
-    if nxt == "-":
-        tok.consume("-")
-        x = factor(tok)
-        return UnaryOp("~", x)
-    elif nxt == "(":
-        tok.consume("(")
-        x = expr(tok)
-        tok.consume(")")
-        return x
-    elif nxt.isalpha():
-        tok.consume(nxt)
-        return Var(nxt)
-    else:
-        tok.consume(nxt)
-        return Num(float(nxt))
-
-
-class Tokenizer:
-    def __init__(self, text):
-        regex = r"\s*([A-Za-z0-9\.]+|.?)"
-        self._tokens = re.finditer(regex, text.rstrip())
-        self._next = next(self._tokens)
-
-    def peek(self) -> str:
-        return self._next.group(1)
-
-    def consume(self, x):
-        if self.peek() != x:
-            raise SyntaxError("Expected " + x)
-        self._next = next(self._tokens)
-
-    def end(self):
-        if self.peek():
-            raise SyntaxError("Extra tokens")
-
-
 class Expr:
     def to_prefix(self) -> str:
         raise NotImplementedError("Abstract method")
@@ -94,8 +30,7 @@ class BinaryOp(Expr):
         y = self._y.eval(ctx)
         op = ops[self._op]
         return op(x, y)
-
-
+    
 class UnaryOp(Expr):
     def __init__(self, op, x: Expr):
         self._op, self._x = op, x
@@ -128,6 +63,70 @@ class Num(Expr):
 
     def eval(self, ctx):
         return self._val
+
+
+class Tokenizer:
+    def __init__(self, text):
+        regex = r"\s*([A-Za-z0-9\.]+|.?)"
+        self._tokens = re.finditer(regex, text.rstrip())
+        self._next = next(self._tokens)
+
+    def peek(self) -> str:
+        return self._next.group(1)
+
+    def consume(self, x):
+        if self.peek() != x:
+            raise SyntaxError("Expected " + x)
+        self._next = next(self._tokens)
+
+    def end(self):
+        if self.peek():
+            raise SyntaxError("Extra tokens")
+
+
+# expr = term {( "+" | "-" ) term}
+# term = factor {( "*" | "/" ) factor}
+# factor = "-" factor | "(" expr ")" | identifier | number
+# (identifiers start with a letter, numbers are float)
+
+# expr = term {( "+" | "-" ) term}
+def expr(tok: Tokenizer) -> Expr:
+    x = term(tok)
+    while tok.peek() in ("+", "-"):
+        op = tok.peek()
+        tok.consume(op)
+        y = term(tok)
+        x = BinaryOp(op, x, y)
+    return x
+
+# term = factor {( "*" | "/" ) factor}
+def term(tok: Tokenizer) -> Expr:
+    x = factor(tok)
+    while tok.peek() in ("*", "/"):
+        op = tok.peek()
+        tok.consume(op)
+        y = factor(tok)
+        x = BinaryOp(op, x, y)
+    return x
+
+# factor = "-" factor | "(" expr ")" | identifier | number
+def factor(tok: Tokenizer) -> Expr:
+    nxt = tok.peek()
+    if nxt == "-":
+        tok.consume("-")
+        x = factor(tok)
+        return UnaryOp("~", x)
+    elif nxt == "(":
+        tok.consume("(")
+        x = expr(tok)
+        tok.consume(")")
+        return x
+    elif nxt.isalpha():
+        tok.consume(nxt)
+        return Var(nxt)
+    else:
+        tok.consume(nxt)
+        return Num(float(nxt))
 
 
 # Tests
