2f685b90cdba76097c00fe4cb1001e7a30269942
Fondamenti di Informatica <144357422+fondinfo@users.noreply.github.com>
2024-05-23 12:23:25 +0200

    Add files via upload

---
+++ c12-preorder.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+@author  Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+"""
+
+class TreeNode:
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left    # TreeNode | None
+        self.right = right  # TreeNode | None
+
+    def __str__(self) -> str:
+        return f"<{self.data} {self.left} {self.right}>"
+
+def insert(tree, val) -> TreeNode:
+    if tree == None:
+        tree = TreeNode(val)
+    elif val < tree.data:
+        tree.left = insert(tree.left, val)
+    elif val > tree.data:
+        tree.right = insert(tree.right, val)
+    return tree
+
+def preorder_traversal(tree: TreeNode): 
+    if tree is not None:
+        print(tree.data)  # visit the root
+        preorder_traversal(tree.left)  # traverse the left subtree
+        preorder_traversal(tree.right)  # traverse the right subtree
+
+t = None
+for v in [7, 5, 5, 9, 6, 2, 3, 11]:
+    t = insert(t, v)
+
+preorder_traversal(t)
+
+
