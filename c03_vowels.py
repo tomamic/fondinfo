71931fef1d1cc2847442d094e81f172c63a7a7ed
Fondamenti di Informatica <144357422+fondinfo@users.noreply.github.com>
2024-05-24 19:02:18 +0200

    Add files via upload

---
+++ c03_vowels.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+@author  Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+"""
+
+line = input("Text? ").lower()
+digits, vowels = 0, 0
+
+for c in line:
+    if "0" <= c <= "9":
+        digits += 1
+    elif c in "aeiou":  # test membership
+        vowels += 1
+
+print(digits, vowels)
