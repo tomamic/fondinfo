a0f7040fa7b3b71c00e5b62f7bfc8cceb0be9a95
Fondamenti di Informatica <144357422+fondinfo@users.noreply.github.com>
2024-05-25 17:24:34 +0200

    Add files via upload

---
+++ c23_fsm.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+@author  Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+"""
+
+alphabet = {"a", "b"}
+states = {"Q0", "Q1", "Q2", "Q3"}
+state = "Q0"
+accepting = {"Q0"}
+transition = {("Q0", "a"): "Q1", ("Q0", "b"): "Q2",
+              ("Q1", "a"): "Q0", ("Q1", "b"): "Q3",
+              ("Q2", "a"): "Q3", ("Q2", "b"): "Q0",
+              ("Q3", "a"): "Q2", ("Q3", "b"): "Q1"}
+
+string = input("String? ")
+for symbol in string:
+    if symbol not in alphabet:
+        raise ValueError(symbol + "∉Σ, Σ=" + str(alphabet))
+
+    new_state = transition.get((state, symbol), None)
+    print((state, symbol), "→", new_state)
+    state = new_state
+    if not state: break  # implicit error trap state
+
+print("Result:", state in accepting)
