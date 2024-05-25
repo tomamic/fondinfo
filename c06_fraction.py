b7e2d3cf877b26607bf8cbd0d9c5221bc94b0353
Fondamenti di Informatica <144357422+fondinfo@users.noreply.github.com>
2024-05-25 11:55:43 +0200

    Add files via upload

---
+++ c06_fraction.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""
+@author  Michele Tomaiuolo - http://www.ce.unipr.it/people/tomamic
+@license This software is free - http://www.gnu.org/licenses/gpl.html
+"""
+
+class Fraction:
+    """Always normalized fraction with positive denominator
+    """
+    def __init__(self, num: int, den: int):
+        if den == 0:  # generate an error
+            raise Exception("Invalid denominator")
+        if den < 0:
+            self._num = -num
+            self._den = -den
+        else:
+            self._num = num
+            self._den = den
+        self._normalize()  # normalize 
+            
+    def _normalize(self):
+        import math
+        gcg = math.gcd(self._num, self._den)
+        self._num = self._num // gcg
+        self._den = self._den // gcg
+    
+    def __str__(self):
+        return f"({self._num}/{self._den})"
+
+    def __add__(self, f: "Fraction") -> "Fraction":
+        """ implements addition of fractions """
+        num = self._num * f._den + f._num * self._den
+        den = self._den * f._den
+        return Fraction(num, den)
+    
+    def __sub__(self, f: "Fraction") -> "Fraction":
+        """ implements subtraction between fractions """
+        num = self._num * f._den - f._num * self._den
+        den = self._den * f._den
+        return Fraction(num, den)
+    
+    def __mul__(self, f: "Fraction") -> "Fraction":
+        """ implements multiplication of fractions """
+        num = self._num * f._num
+        den = self._den * f._den
+        return Fraction(num, den)
+    
+    def __truediv__(self, f: "Fraction") -> "Fraction":
+        """ implements division between fractions """        
+        num = self._num * f._den
+        den = self._den * f._num
+        return Fraction(num, den)    
+
+def main():
+    f1 = Fraction(10, 4)
+    f2 = Fraction(-7, 8)
+    print(f1 + f2)      # (13/8)
+    print(f1 - f2)      # (27/8)
+    print(f1 * f2)      # (-35/16)
+    print(f1 / f2)      # (-20/7)
+
+
+if __name__ == "__main__":
+    main()
