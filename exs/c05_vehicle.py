ccecd92708e839e165da9c4e6548d72d4aba6784
tomamic <tomamic@ce.unipr.it>
2024-06-22 18:18:36 +0200

    license

--- exs/c05_vehicle.py
+++ exs/c05_vehicle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-@author  Michele Tomaiuolo - https://tomamic.github.io
+@author  Michele Tomaiuolo - https://tomamic.github.io/
 @license This software is free - https://opensource.org/license/mit
 """
 import sys; sys.path.append("../")
