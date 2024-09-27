ebdb9d5ce7cdfaa531669023ef312d2dd8f06c7f
Michele Tomaiuolo <tomamic@ce.unipr.it>
2024-09-27 09:00:54 +0200

    Add files via upload

---
+++ exs/c02_polyline.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+@author  Michele Tomaiuolo - https://tomamic.github.io/
+@license This software is free - https://opensource.org/license/mit
+"""
+
+import sys; sys.path.append("../")
+import g2d
+from random import randrange
+
+W, H = 480, 360
+g2d.init_canvas((W, H))
+g2d.set_color((0, 0, 0))
+
+n = int(g2d.prompt("How many segments? "))
+
+src = (randrange(W), randrange(H))
+for _ in range(n):
+    dst = (randrange(W), randrange(H))
+    g2d.draw_line(src, dst)
+    src = dst
+
+g2d.main_loop()
