bde16aa3182379c561554e2f8d698efd0cef47b5
tomamic <tomamic@ce.unipr.it>
2024-10-28 17:03:03 +0100

    simpler

--- c09_fifteen.py
+++ c09_fifteen.py
@@ -9,30 +9,44 @@ from random import choice
 
 class Fifteen(BoardGame):
     def __init__(self, w: int, h: int):
-        self._w, self._h = w, h
-        self._won = True  # sorted board tiles: [1 2 ... 14 15 0]
-        self._bd = bd = list(range(1, w * h)) + [0]
+        # init board with sorted tiles: [1 2 ... 14 15 0]
+        self._bd = list(range(1, w * h)) + [0]
         self._x0, self._y0 = w - 1, h - 1  # blank
-        while w * h > 1 and bd[-1] != 1: # random walk of blank tile
+        self._won = True
+        self._w, self._h = w, h
+
+        # then, random walk of the blank tile, until most tiles change
+        while w * h > 1 and self._bd[-1] != 1:
             dx, dy = choice([(0, -1), (1, 0), (0, 1), (-1, 0)])
             self.play(self._x0 + dx, self._y0 + dy, "")
 
     def play(self, x: int, y: int, action: str):
         w, h, bd, x0, y0 = self._w, self._h, self._bd, self._x0, self._y0
-        if 0<=x<w and 0<=y<h and abs(x-x0) + abs(y-y0) == 1:
-            bd[y0*w + x0], bd[y*w + x] = bd[y*w + x], 0  # swap tiles
+        if 0 <= x < w and 0 <= y < h and abs(x-x0) + abs(y-y0) == 1:
+            bd[y0 * w + x0] = bd[y * w + x]  # swap tiles
+            bd[y * w + x] = 0
             self._x0, self._y0 = x, y  # blank
-            self._won = all(v in (0, i+1) for i, v in enumerate(bd))
+            self._won = self._sorted()  # hint: use `all`, instead
+
+    def _sorted(self) -> bool:
+        for i in range(self._w * self._h - 1):
+            if self._bd[i] != i + 1:
+                return False
+        return True
 
     def read(self, x: int, y: int) -> str:
         w, h, bd = self._w, self._h, self._bd
-        return str(bd[y*w + x]) if 0<=x<w and 0<=y<h and bd[y*w + x] else ""
+        if 0 <= x < w and 0 <= y < h and bd[y * w + x]:
+            return str(bd[y*w + x])
+        return ""
 
     def finished(self) -> bool:
         return self._won
 
     def status(self) -> str:
-        return "Puzzle solved!" if self._won else "Playing"
+        if self._won:
+            return "Puzzle solved!"
+        return "Playing"
 
     def cols(self) -> int:
         return self._w
@@ -43,6 +57,6 @@ class Fifteen(BoardGame):
 
 if __name__ == "__main__":
     from boardgamegui import gui_play
-    game = Fifteen(2, 2)
+    game = Fifteen(4, 4)
     ##console_play(game)
     gui_play(game)
